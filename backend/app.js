8d4d14c519d0a9a2567c8b1930fb66eaa8fbc61e
laurent <soyezlaurent@gmail.com>
2022-10-31 19:02:29 +0100

    1er git : implamentation des fichier

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const path = require("path");
+
+const saucesRoutes = require("./routes/sauces");
+const userRoutes = require("./routes/user");
+
+mongoose
+  .connect(
+    "mongodb+srv://LaurentS87:Lol999250@cluster0.x2m238x.mongodb.net/P6?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", saucesRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+module.exports = app;

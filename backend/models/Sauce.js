2208d2c6b4b8226d07ae0174c0aa8d90bfa51fa0
laurent <soyezlaurent@gmail.com>
2022-11-06 11:22:16 +0100

    add mongosse-errors+ajout trim form ctrl sauce

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,11 +1,12 @@
+const MongooseErrors = require("mongoose-errors");
 const mongoose = require("mongoose");
 
 const saucesSchema = mongoose.Schema({
   userId: { type: String, required: true },
-  name: { type: String, required: true },
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
+  name: { type: String, required: true, trim: true },
+  manufacturer: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
+  mainPepper: { type: String, required: true, trim: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
   likes: { type: Number, default: 0 },
@@ -13,5 +14,7 @@ const saucesSchema = mongoose.Schema({
   usersLiked: { type: [String], default: [] },
   usersDisliked: { type: [String], default: [] },
 });
+
+saucesSchema.plugin(MongooseErrors);
 
 module.exports = mongoose.model("Sauce", saucesSchema);
